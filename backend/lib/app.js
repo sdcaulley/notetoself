93a76f8e6e8bc8af5b6128f335219911e88a1869
sdcaulley <stephanie.caulley@gmail.com>
2019-05-17 21:57:58 +0200

    server, db, and user schema set up

---
+++ backend/lib/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+//const users = require('./routes/user-route');
+
+const app = express();
+app.use(cors());
+app.use(passport.initialize());
+require('./passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//app.use('/', users);
+
+app.get('/', function(req, res) {
+    res.send('hello');
+});
+
+module.exports = app;
---
+++ backend/test/_before.js
@@ -0,0 +1,11 @@
+process.env.MONGODB_URI = 'mongodb://localhost:27017/n2s_test';
+require('../lib/db');
+const mongoose = require('mongoose');
+
+before((done) => {
+    const drop = () => mongoose.connection.dropDatabase(() =>{
+        done();
+    });
+    if(mongoose.connection.readyState === 1) drop();
+    else mongoose.connection.on( 'open', drop);
+});
