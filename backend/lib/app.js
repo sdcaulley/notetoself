af6eeddc147d86b6ab7bd65068a0e0287baa4570
sdcaulley <stephanie.caulley@gmail.com>
2019-05-17 22:02:04 +0200

    added login validation

--- backend/lib/app.js
+++ backend/lib/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-//const users = require('./routes/user-route');
+const users = require('./routes/user-route');
 
 const app = express();
 app.use(cors());
@@ -12,7 +12,7 @@ require('./passport')(passport);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//app.use('/', users);
+app.use('/', users);
 
 app.get('/', function(req, res) {
     res.send('hello');
